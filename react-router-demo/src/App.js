ab39def96d94c731f669f5c504dec0a21f582222
Vang Xiong <vangx709@gmail.com>
2023-01-20 09:22:11 -0900

    Navigating ,NoMatchRoute ,NestedRoute , IndexRoute

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,15 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import { About } from "./components/About";
+import { FeaturedProducts } from "./components/FeaturedProducts";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { NewProducts } from "./components/NewProducts";
+import { NoMatch } from "./components/NoMatch";
+import { OrderSummary } from "./components/OrderSummary";
+import { Products } from "./components/Products";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
+          <Route path="feature" element={<FeaturedProducts />}></Route>
+          <Route path="new" element={<NewProducts />}></Route>
+        </Route>
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
