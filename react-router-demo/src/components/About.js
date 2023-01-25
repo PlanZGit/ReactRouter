9f6c42b14fa75803822243122844f0f2a61df453
Vang Xiong <vangx709@gmail.com>
2023-01-24 15:35:32 -0900

    Added Lazy Loading

--- react-router-demo/src/components/About.js
+++ react-router-demo/src/components/About.js
@@ -1,5 +1,24 @@
 import React from "react";
 
-export const About = () => {
-  return <div>About Us</div>;
+const About = () => {
+  return (
+    <div>
+      <h3>About Us</h3>
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details. About Us Details. About Us Details.
+      About Us Details. About Us Details.
+    </div>
+  );
 };
+
+export default About;
