f1e5830390c3540d8e5c1f930ce6f0e1f066ff34
Vang Xiong <vangx709@gmail.com>
2023-01-25 18:50:53 -0900

    Authentication and Protected Routes

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 // import { About } from "./components/About";
 import { Admin } from "./components/Admin";
+import { AuthProvider } from "./components/auth";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { Home } from "./components/Home";
+import { Login } from "./components/Login";
 import { Navbar } from "./components/Navbar";
 import { NewProducts } from "./components/NewProducts";
 import { NoMatch } from "./components/NoMatch";
 import { OrderSummary } from "./components/OrderSummary";
 import { Products } from "./components/Products";
+import { Profile } from "./components/Profile";
+import { RequireAuth } from "./components/RequireAuth";
 import { UserDetails } from "./components/UserDetails";
 import { Users } from "./components/Users";
 const LazyAbout = React.lazy(() => import("./components/About"));
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -37,9 +41,18 @@ function App() {
           <Route path=":userId" element={<UserDetails />} />
           <Route path="admin" element={<Admin />} />
         </Route>
+        <Route
+          path="profile"
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="login" element={<Login />}></Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
-    </>
+    </AuthProvider>
   );
 }
 
